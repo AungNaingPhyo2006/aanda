cfa2409f302a6016721f657d278404c5d2e10302
AungNaingPhyo2006 <115768795+AungNaingPhyo2006@users.noreply.github.com>
2024-09-26 23:43:50 +0630

    Update plan

--- app/plan/page.tsx
+++ app/plan/page.tsx
@@ -4,23 +4,23 @@ export default function Plan() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-b from-[#FFD700] to-[#FFA500] p-2">
     <div className="bg-white/80 p-6 rounded-lg shadow-lg w-full max-w-lg overflow-y-auto h-[90vh] scrollbar-hide">
-      <h1 className="text-2xl font-bold text-center text-[#4C5933] mb-6">မင်္ဂလာ အစီအစဥ်</h1>
-      <h1 className="text-2xl font-bold text-center text-[#383B42] mt-2">..................</h1>
+      <h1 className="text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl font-bold text-center text-[#4C5933] mb-6">မင်္ဂလာ အစီအစဥ်</h1>
+      <h1 className="text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl font-bold text-center text-[#383B42] mt-2">..................</h1>
 
       {/* Scrollable Content */}
       {/* Wedding Plan Details */}
       <div className="text-center space-y-4 mt-6">
-          <p className="text-2xl font-bold   text-gray-900">
+          <p className="text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl font-bold   text-gray-900">
           မင်္ဂလာ နေ့ရက်
           </p>
           <p className="text-lg text-[#462048]">၁၃၈၆ ခုနှစ်၊ တန်ဆောင်မုန်း လပြည့်ကျော်(၉)ရက်၊ ၂၄.၁၁.၂၀၂၄(တနင်္ဂနွေနေ့)။<br/></p>
           <div className='h-12'/>
-          <p className="text-2xl font-bold   text-gray-900">
+          <p className="text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl font-bold   text-gray-900">
           မင်္ဂလာ အချိန်
           </p>
           <p className="text-lg  text-[#462048]">နံနက်( ၈ ) နာရီ မှ ( ၁၀ ) နာရီ အထိ။<br/></p>
           <div className='h-12'/>
-          <p className="text-2xl font-bold   text-gray-900">
+          <p className="text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl font-bold   text-gray-900">
           မင်္ဂလာ နေရာ
           </p>
           <p className="text-lg  text-[#462048]">အမှတ်(၄၂၆)၊ အုတ်ဖိုတန်းလမ်း၊ အောက်ကျင်းရပ်၊ (ဦးဖိုးစိန်) + ဒေါ်ခင်ညိုဝင်း မိသားစုတို့၏ နေအိမ်သို့။<br/></p>
