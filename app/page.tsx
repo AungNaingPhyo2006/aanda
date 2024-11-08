79d0b9ccd6e634c0ef149b4e6092157ee1a39ab2
AungNaingPhyo2006 <115768795+AungNaingPhyo2006@users.noreply.github.com>
2024-11-08 09:09:21 +0630

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,11 +9,13 @@ export default function Home() {
         </h1>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-        <Link
+        {/* <Link
           className="rounded-full border border-solid border-white/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-pink-500 hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
           href="/about">
           ကြည့်ရှုမည်
-        </Link>
+        </Link> */}
+        <Link className="rounded-full border border-solid border-white/[.08] hover:bg-pink-500 hover:border-transparent  font-bold text-white py-2 px-4 rounded-md hover:bg-blue-600" href="/about"> ကြည့်ရှုမည်</Link>
+
         </div>
       
       </main>
