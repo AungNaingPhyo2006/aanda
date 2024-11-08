69447305519909d6d6f6ed871b61c0106af9e7d4
AungNaingPhyo2006 <115768795+AungNaingPhyo2006@users.noreply.github.com>
2024-11-08 15:33:41 +0630

    Update page.tsx

--- app/love/page.tsx
+++ app/love/page.tsx
@@ -9,7 +9,7 @@ export default function Love() {
           <p className="sm:text-lg md:text-xl lg:text-xl xl:text-xl text-[#FFFDD0]">ကြင်နာဖော် ကြင်နာဖက်နဲ့</p>
           <p className="sm:text-lg md:text-xl lg:text-xl xl:text-xl text-[#FFFDD0]">လက်တွဲဖို့ ရည်ရွယ်ခဲ့တဲ့</p>
           <p className="sm:text-lg md:text-xl lg:text-xl xl:text-xl text-[#FFFDD0]">ကိုယ့်ရဲ့ဆန္ဒလေးတစ်ခု</p>
-         <p className="sm:text-lg md:text-xl lg:text-xl xl:text-xl text-text-[#FFFDD0]">မင်းနဲ့တွေ့မှ ပြည့်ခဲ့ပြီ....။</p>
+         <p className="sm:text-lg md:text-xl lg:text-xl xl:text-xl text-[#FFFDD0]">မင်းနဲ့တွေ့မှ ပြည့်ခဲ့ပြီ....။</p>
       <div className="flex gap-4 items-center flex-col sm:flex-row">
       {/* <Link
         className="rounded-full border border-solid border-white/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-pink-500 hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
